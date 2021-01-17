c68fcc8e8cd24123edf2aaf7a75b9dfcd7e0b38a
anuragmuthyam <anu.drumcoder@gmail.com>
2021-01-16 20:21:28 -0600

    first commit

---
+++ src/main/java/com/lomboktodelombok/LombokToDelombokApplication.java
@@ -0,0 +1,13 @@
+package com.lomboktodelombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokToDelombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokToDelombokApplication.class, args);
+	}
+
+}
